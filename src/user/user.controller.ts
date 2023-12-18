81aab8a934b61710e86c4a88bd0e5029daa0a88e
Gabriel de Godoy Russo <gabriel.godoy@bcare.com.br>
2023-12-18 17:51:34 -0300

    feat: Adicionando user service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,21 @@
+import { UserService } from './user.service';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
+    constructor(private readonly userService: UserService) {}
 
     @Post()
     async createUser(
-        @Body() createUser: CreateUserDto
+        @Body() createUserDto: CreateUserDto
     ) {
-        return {
-            ...createUser,
-            password: undefined
-        };
+        return await this.userService.createUser(createUserDto);
+    }
+
+    @Get()
+    async getAllUsers(): Promise<User[]> {
+        return await this.userService.getAllUsers();
     }
 }
